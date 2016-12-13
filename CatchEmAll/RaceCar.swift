a90408f923c5047e13110d0e9b5d0d292b42a390
Lloyd1nSpace <lloyd1nspace@icloud.com>
2016-12-13 12:24:17 -0500

    Coming along most classes done

--- CatchEmAll/RaceCar.swift
+++ CatchEmAll/RaceCar.swift
@@ -7,3 +7,46 @@
 //
 
 import Foundation
+
+class RaceCar: Car {
+    
+    let driver: String
+    var sponsors: [String]
+    
+    init(name: String, weight: Double, maxSpeed: Double, transmission: Transmission, cylinders: Int, milesPerGallon: Double, driver: String, sponsors: [String]) {
+        self.driver = driver
+        self.sponsors = sponsors
+        super.init(name: name, weight: weight, maxSpeed: maxSpeed, transmission: transmission, cylinders: cylinders, milesPerGallon: milesPerGallon)
+    }
+    
+    override func accelerate() {
+        self.speed += self.speed * 1/5
+    }
+    
+    override func decelerate() {
+        self.speed -= self.speed * 1/5
+    }
+    
+    func driftRight() {
+        if self.speed > 0 {
+            self.speed /= 4
+            if self.heading == 360 {
+                self.heading = 0
+            } else {
+                self.heading += 90
+            }
+        }
+        
+    }
+    
+    func driftLeft() {
+        if self.speed > 0 {
+            self.speed /= 4
+            if self.heading == 0 {
+                self.heading = 270
+            } else {
+                self.heading -= 90
+            }
+        }
+    }
+}
